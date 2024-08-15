93889c266129fb6acaa84a9cbbee1fe3a94939de
Alejandro Caceres <ale@Alejandros-MacBook-Pro.local>
2024-08-15 10:30:27 +0200

    Update experience

--- src/app/sections/Experience/experience.ts
+++ src/app/sections/Experience/experience.ts
@@ -9,10 +9,18 @@ export interface Experience {
 
 const experience: Experience[] = [
     {
+        startMonth: "Mar 2024",
+        organisation: "Social Currant",
+        location: "Washing DC, USA",
+        description: "Scale the app to handle media content provided by creators, implement modern engineering best practices and discuss product challenges",
+        position: "Lead Fullstack Engineer",
+    },
+    {
         startMonth: "May 2021",
+        endMonth: "Mar 2024",
         organisation: "Workfinder",
         location: "London, UK",
-        description: "Worked heavily on django and react codebase to help people fix their skill gaps and organisations reach their recruiting, DEI targets and other challenges",
+        description: "Worked heavily on django and react codebase to help people fix their skill gaps and organisations reach their recruiting, DEI targets and other goals",
         position: "Fullstack Engineer",
     },
     {
